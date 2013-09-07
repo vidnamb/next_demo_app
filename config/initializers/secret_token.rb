4a6de6e226127dc4637ec96309b4f56abcb52f40
Vidya Nambiar <vnambiar@andrew.cmu.edu>
2013-09-07 14:35:36 -0700

    Initial respository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NextDemoApp::Application.config.secret_key_base = '84b7c2643f8b91164822fd258bc1897a6f20a783cd9a9c7beb687528b3149b41b111bb8dc4270603c02b5283dd9270d892cf982640b421afcd1fd10e83877004'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NextDemoApp::Application.config.session_store :cookie_store, key: '_next_demo_app_session'
